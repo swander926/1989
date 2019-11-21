3325a7ff0eb78eef9eef55e4b8e45902a58e0abb
Nathanael Swander <swander926@yahoo.com>
2019-11-21 00:44:28 -0500

    easter egg

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,8 @@
 import React, { useState, useEffect } from 'react' //why can we pass 2? why not use component? what exactly are these?
 import Movies from './components/Movies'
+import gettingold from './images/gettingold.png'
+import easteregg from './images/easter-egg.gif'
+import fluxcapacitor from './images/fluxcapacitor.jpg'
 
 import axios from 'axios'
 
@@ -21,17 +24,23 @@ const App = () => {
 
   return (
     <>
+      <h1> 1989 </h1>
+      <div id="fluxContainer">
+        <div id="fluxBox">
+          <img src={fluxcapacitor} height="150" width="150" />
+        </div>
+      </div>
       <div id="introContainer">
         <div id="introBox">
           <p id="introParagraph">
             It's 1989. The internet has just come online!! It is the year before
-            the best decade of the 20th century! Film is about to hit it's
-            prime, when movie plots were still organic, original, and unique.
-            The 21st century spawned a new age of film where nearly every movie
-            produced became a remake of ideas from the past :( Where is the
-            creativity, millenials? So, let's pop a VHS in the player to bring
-            back old memories! Or, you can browse this page to see a list of
-            some of the best movies in 1989. Enjoy (~):)
+            the best decade of the 20th century! Film is about to hit its prime
+            while movie plots are still organic, original, and unique. The 21st
+            century spawned a new age of film where nearly every movie produced
+            became a remake of ideas from the past :( Where is the creativity,
+            millenials? So, let's pop a VHS in the player and reminisce old
+            memories! Or, you can browse this page to see a list of some of the
+            best movies in 1989. Enjoy (~):)
           </p>
         </div>
       </div>
@@ -47,6 +56,19 @@ const App = () => {
           )
         })}
       </ul>
+      <div id="easterContainer">
+        <div id="easterBox">
+          <br></br>
+          <img src={easteregg} height={90} width={90} />
+          <br></br>
+          <br></br>
+          <img src={gettingold} height={400} width={350} />
+          <br></br>
+          <br></br>
+          <img src={easteregg} height={90} width={90} />
+          <br></br>
+        </div>
+      </div>
     </>
   )
 }
